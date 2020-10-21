1305bfbf803160778edb9e36bbe7c164a6db04bc
msteele11101 <msteele11101@gmail.com>
2020-10-21 06:31:26 -0700

    Updates

---
+++ precache-manifest.3447d5f3f7d3913d7469cc4c315f5ea8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb7251a0ff7b1e5283769f05c4aabd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c47f065bc26337a7b9",
+    "url": "/static/css/main.5196e66f.chunk.css"
+  },
+  {
+    "revision": "567cb456a22df0e58758",
+    "url": "/static/js/2.f6c98796.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f6c98796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c47f065bc26337a7b9",
+    "url": "/static/js/main.a0d82f9b.chunk.js"
+  },
+  {
+    "revision": "85d367d40f4625aef104",
+    "url": "/static/js/runtime-main.1281186e.js"
+  }
+]);
